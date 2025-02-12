5555206c65e5e1ae3117a6bc3e55c0471c30ae04
Sara Jaber <sjaber@c2r2s15.42amman.com>
2025-02-12 19:18:25 +0300

    Norm updates

--- client.c
+++ client.c
@@ -6,73 +6,68 @@
 /*   By: sjaber <sjaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 13:50:00 by sjaber            #+#    #+#             */
-/*   Updated: 2025/02/10 17:08:29 by sjaber           ###   ########.fr       */
+/*   Updated: 2025/02/12 19:14:33 by sjaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static int g_ack = 0;
+static int	g_ack = 0;
 
-static void ack_handler(int sig)
+static void	ack_handler(int signal)
 {
-        (void)sig;
-        g_ack = 1;
+	if (signal == SIGUSR1)
+		g_ack = 1;
 }
 
-static void send_char(char c, pid_t pid)
+static void	send_char(char c, pid_t pid)
 {
-        int     i;
+	int	i;
 
-        i = 8;
-        while (i > 0)
-        {
-                g_ack = 0;
-                if((c >> i) & 1)
-                        kill(pid, SIGUSR2);
-                else
-                        kill(pid, SIGUSR1);
-                while(!g_ack)
-                        usleep(300);
-                i--;
-        }
+	i = 7;
+	while (i >= 0)
+	{
+		g_ack = 0;
+		if ((c >> i) & 1)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		while (!g_ack)
+			;
+		i--;
+	}
 }
 
-static int valid_pid(char *pid)
+static int	valid_pid(char *pid)
 {
-        int     i;
-
-        i = 0;
-        while (pid[i])
-        {
-                if (!isdigit(pid[i]))
-                return (0);
-                i++;
-        }
-        return (1);
+	while (*pid)
+	{
+		if (!isdigit(*pid))
+			return (0);
+		pid++;
+	}
+	return (1);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-        pid_t   pid;
+	pid_t	pid;
+	char	*input_string;
 
-        if(argc != 3)
-        {
-                ft_printf("Usage: ./client [PID] [Message]\n");
-                return (1);
-        }
-
-        if(!valid_pid(argv[1]))
-        {
-            ft_printf("Invalid PID\n");
-            exit(1);
-        }
-        pid = ft_atoi(argv[1]);
-        signal(SIGUSR1, ack_handler);
-        while(*argv[2])
-        {
-                send_char(*argv[2], pid);
-                argv[2]++;
-        }
-        send_char('\0', pid);
+	if (argc != 3)
+		return (1);
+	if (!valid_pid(argv[1]))
+	{
+		ft_printf("Invalid PID\n");
+		exit(1);
+	}
+	pid = ft_atoi(argv[1]);
+	input_string = argv[2];
+	signal(SIGUSR1, ack_handler);
+	while (*input_string)
+	{
+		send_char(*input_string, pid);
+		input_string++;
+	}
+	send_char('\0', pid);
 }
